68f86f9c056430c9d5c7d13bf90ae99eea551edc
Tsutomu Kawamura <cognitom@gmail.com>
2018-03-01 01:25:01 +0000

    Add dist dir to git

---
+++ dist/index.js
@@ -0,0 +1,1 @@
+var a="431412 44 45 46 47 48 49 5 6 7 8 90 91 92 93",b="4980 4988503 51 52 53 54 55 56 57 58 59 6 7 8 90 91 92 93",data={a:a,b:b},index=(a,e="A")=>data[e.toLowerCase()].split(" ").find(e=>0===a.indexOf(e))?"w":"e";export default index;
